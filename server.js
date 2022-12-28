fcdb53b70c138f12737a0ec9943c27615782febe
yuwen <yuwen.azulejos@gmail.com>
2022-12-28 16:34:09 +0800

    feat: store session to cookie using cookie-session

--- server.js
+++ server.js
@@ -5,32 +5,54 @@ const path = require('path');
 const express = require('express');
 const helmet = require('helmet');
 const passport = require('passport');
+const cookieSession = require('cookie-session');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 // require('https').globalAgent.options.rejectUnauthorized = false;
 
 require('dotenv').config();
 
 const config = {
-  clientID: process.env.CLIENT_ID,
-  clientSecret: process.env.CLIENT_SECRET,
+  CLIENT_ID: process.env.CLIENT_ID,
+  CLIENT_SECRET: process.env.CLIENT_SECRET,
+  COOKIE_KEY_1: process.env.COOKIE_KEY_1,
+  COOKIE_KEY_2: process.env.COOKIE_KEY_2,
 };
 
 const GOOGLE_AUTH_OPTIONS = {
-  clientID: config.clientID,
+  clientID: config.CLIENT_ID,
   clientSecret: config.CLIENT_SECRET,
   callbackURL: '/auth/google/callback',
 };
 
 const verifyCallback = (accessToken, refreshToken, profile, done) => {
+  console.log('profile', profile);
   done(null, profile);
 };
 
 passport.use(new GoogleStrategy(GOOGLE_AUTH_OPTIONS, verifyCallback));
 
+// save user data to session, can only choose non-sensitive data
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+// read the session from the cookie
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
+});
+
 const app = express();
 
 app.use(helmet());
+app.use(
+  cookieSession({
+    name: 'session',
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [config.COOKIE_KEY_1, config.COOKIE_KEY_2],
+  })
+);
 app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
@@ -67,7 +89,7 @@ app.get(
   passport.authenticate('google', {
     failureRedirect: '/auth/failure',
     successRedirect: '/',
-    session: false,
+    session: true,
   }),
   (req, res) => {
     console.log('google called us back!');
